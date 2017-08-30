c5e8db8cb968f868fdbb7b7c0cea0a59dff35ab6
vvvVivec <31455696+vvvvivec@users.noreply.github.com>
2017-08-30 08:46:34 -0400

    Add files via upload

---
+++ control-checkgroups.ps1
@@ -0,0 +1,55 @@
+﻿# This script pulls a list of all current ad groups a user is a member of 
+# $control - determines action of script
+# $userid - user n# to search
+# $vector - stu- branch to look for matches
+
+# define parameters
+param (
+    $control,
+    $userid,
+    $vector
+)
+
+# construct help message 
+$helpmsg = "`nSyntax: .\control-checkgroups.ps1 <control> <userid> <vector>"
+$helpmsg += "`n`ncontrol - valid options: help, check, verify"
+$helpmsg += "`nhelp - display help message"
+$helpmsg += "`ncheck - check what groups user is a member of"
+$helpmsg += "`nverify - verify user's full name"
+$helpmsg += "`n`nuserid - n# of user to check"
+$helpmsg += "`nvector - filter to apply, e.g. 'stu-'"
+
+# if $control == check , check groups $userid belongs to
+if ($control -like 'check')
+{
+    # if no vector is supplied , get all groups
+    if (!$vector)
+    {
+    Get-ADPrincipalGroupMembership -Identity $userid | Select -Property name 
+    }
+
+    # if vector is supplied , get groups with grep on vector 
+    else
+    {
+    Get-ADPrincipalGroupMembership -Identity $userid | Select -Property name | sls $vector
+    }
+}
+
+# if $control == verify , get $userid's first and last name
+if ($control -like 'verify')
+{
+    Get-ADUser -Identity $userid | Select -Property 'givenname', surname
+}
+
+# if $control == help , display help message 
+if ($control -like 'help')
+{
+    Write-Host $helpmsg
+}
+
+# if no valid control supplied , display error along with syntax for help message
+elseif (!$control)
+{
+    Write-Host "Error, invalid control provided`nProper syntax: .\control-checkgroups.ps1 <control> <userid> <vector>"
+    Write-Host "For information, use: .\control-checkgroups.ps1 help"
+}
---
+++ control-sg.ps1
@@ -0,0 +1,118 @@
+﻿# control-sg 
+# This script is used to add/remove members to the groups appropriate for a designated SG position 
+# Author: Brian D. 
+# Date: 08/18/17
+
+# PARAMETERS: 
+
+# $control: add , remove , check , purge , help
+# add - adds specified user to vector group
+# remove - removes specified user from vector group
+# check - see what SG groups user belongs to
+# purge - remove user from all SG groups
+# help - display help message
+
+# $userid: user account to manipulate 
+
+# $vector: group to add/remove user to/from, utilizes last sections of STU-SG-SGAXXXXX, e.g. 'sgaag' 
+
+# $flag: flag for DL AD group modification rather than standard STU-SG-* format 
+param
+(
+    [string]$control,
+    [string]$userid,
+    [string]$vector,
+    [string]$flag
+)
+
+# get list of members for $destination AD group to check against
+function checkMembership()
+{
+    Get-ADGroupMember -Identity $destination -Recursive | Select -ExpandProperty Name
+}
+
+# help string for 'help' control
+$helpString = "Syntax - .\control-sg.ps1 <control> <userid> <vector> <flag>`nControl - add, remove, purge, check, help" 
+$helpString += "`nUserid - n# of user to add/remove`nVector - relative group to add/remove from , use SGA*"
+
+# destination = AD group matched based on supplied $vector
+# groups - gets a list of all STU-SG-* groups 
+if ($flag -like 'dl')
+{
+    $destination = (Get-ADGroup -Filter "name -like 'STU-SGA-$vector*'").DistinguishedName
+    $groups = (Get-ADGroup -Filter "name -like 'STU-SGA-*'").DistinguishedName    
+}
+else
+{
+    $destination = (Get-ADGroup -Filter "name -like 'STU-SG-$vector'").DistinguishedName
+    $groups = (Get-ADGroup -Filter "name -like 'STU-SG-*'").DistinguishedName
+}
+
+# logic for 'add' control
+# add user, check to make sure they were successfully added and write message to confirm
+if ($control -like 'add')
+{
+    Add-ADGroupMember -Identity "$destination" -Members $userid
+    $members = checkMembership
+    if ($members -contains $userid)
+    {
+        Write-Host "User $userid successfully added to $vector`n$destination"
+    }
+}
+
+# logic for 'remove' control
+# check if $userid is member of designated group, if so remove and write message to confirm
+# if not a member, write message stating user not found
+if ($control -like 'remove')
+{
+    $members = checkMembership
+    if ($members -contains $userid)
+    {
+        Write-Host "Removing User $userid from $vector`n$destination"
+        Remove-ADGroupMember -Identity "$destination" -Members $userid -Confirm
+    }    
+    else
+    {
+        Write-Host "User $userid not found in $vector"
+    }
+}
+
+# logic for 'check' control
+# check against master list of SG AD groups, $groups 
+# check each group to see if $Userid is a member
+# write out message for each valid membership
+if ($control -like 'check')
+{
+    foreach ($group in $groups)
+    {
+        $destination = $group 
+        $members = checkMembership
+        if ($members -contains $userid)
+        {
+            Write-Host "User $userid found in $group.DistinguishedName"
+        }
+    }
+}
+
+# logic for 'purge' control
+# check which SG groups $userid belongs to and remove $userid from each
+if ($control -like 'purge')
+{
+    foreach ($group in $groups)
+    {
+        $destination = $group
+        $members = checkMembership
+        if ($members -contains $userid)
+        {
+            Write-Host "Removing User $userid from $group"
+            Remove-ADGroupMember -Identity "$group" -Members $userid -Confirm
+        }
+    }
+}
+
+# logic for 'help' control
+# write out help string
+if (($control -like 'help') -or ($control -like ''))
+{
+    Write-Host $helpString    
+}
